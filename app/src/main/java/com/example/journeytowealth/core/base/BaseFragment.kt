a46097543585078a18c236a673bc58d4575ec8c0
DESKTOP-M1UFE2V\user <tvroom9102@gmail.com>
2026-02-27 15:37:08 +0900

    Toolbar 완성

--- app/src/main/java/com/example/journeytowealth/core/base/BaseFragment.kt
+++ app/src/main/java/com/example/journeytowealth/core/base/BaseFragment.kt
@@ -1,11 +1,13 @@
 package com.example.journeytowealth.core.base
 
+
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.viewbinding.ViewBinding
+import com.example.journeytowealth.R
 
 abstract class BaseFragment<VB : ViewBinding>(
     private val inflate: (LayoutInflater, ViewGroup?, Boolean) -> VB
@@ -22,6 +24,10 @@ abstract class BaseFragment<VB : ViewBinding>(
         savedInstanceState: Bundle?
     ): View {
         _binding = inflate(inflater, container, false)
+
+        binding.root.setBackgroundColor(
+            requireContext().getColor(R.color.bg_main)
+        )
         return binding.root
     }
 
--- app/src/main/java/com/example/journeytowealth/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/journeytowealth/ui/main/MainActivity.kt
@@ -1,24 +1,47 @@
 package com.example.journeytowealth.ui.main
 
 import android.os.Bundle
+import android.view.View
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.example.journeytowealth.R
 import com.example.journeytowealth.core.base.BaseActivity
 import com.example.journeytowealth.databinding.ActivityMainBinding
 
+
+/**
+ * Todo:
+ * 1. MainActivity :
+ * 1) Toolbar :
+ * 1-1) 추후 시간 될 경우 RecyclerView로 menu 추가하기 (Toolbar UI 개선)
+ */
 class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
+
+    var currentToolbarStatus = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+
+        // 1. Toolbar 부분 세팅
+        setToolbarUiAndAction()
+
+        // 2. BottomNavigation 세팅
         val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.nav_host)
-                    as NavHostFragment
-
+            supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment
         val navController = navHostFragment.navController
+        binding.bottomNavigationMain.setupWithNavController(navController)
+    }
 
-        binding.bottomNavigationMain
-            .setupWithNavController(navController)
+    fun setToolbarUiAndAction() {
+        binding.toolbarMain.ivMenuToolbar.setOnClickListener {// 메뉴 클릭시
+            currentToolbarStatus = !currentToolbarStatus
+            if (currentToolbarStatus) {
+                binding.menu1Main.visibility = View.VISIBLE
+            } else {
+                binding.menu1Main.visibility = View.GONE
+            }
+
+        }
     }
 }

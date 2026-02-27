9ad6d55e47b978f7e2586c5ea558e275f82882b6
DESKTOP-M1UFE2V\user <tvroom9102@gmail.com>
2026-02-27 14:41:12 +0900

    (1) BaseActivity, BaseFragment 세팅 (2) ViewBinding 세팅

--- app/src/main/java/com/example/journeytowealth/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/journeytowealth/ui/main/MainActivity.kt
@@ -1,26 +1,16 @@
 package com.example.journeytowealth.ui.main
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.example.journeytowealth.R
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.example.journeytowealth.core.base.BaseActivity
+import com.example.journeytowealth.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0)
-            insets
-        }
 
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host)
@@ -28,7 +18,7 @@ class MainActivity : AppCompatActivity() {
 
         val navController = navHostFragment.navController
 
-        val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNavigation_main)
-        bottomNav.setupWithNavController(navController)
+        binding.bottomNavigationMain
+            .setupWithNavController(navController)
     }
 }

49083ce1b07f3910367218ceca4740a649ef3ab7
DESKTOP-M1UFE2V\user <tvroom9102@gmail.com>
2026-02-27 11:52:10 +0900

    1. bottom navigation 및 필요한 부분들 추가 2. fragment 미리 생성

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,4 +46,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    // Bottom Navigation
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
 }
--- app/src/main/java/com/example/journeytowealth/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/journeytowealth/ui/main/MainActivity.kt
@@ -5,7 +5,10 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.journeytowealth.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,5 +20,11 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
+        val navController = findNavController(R.id.nav_host)
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNavigation_main)
+
+        bottomNav.setupWithNavController(navController)
     }
 }

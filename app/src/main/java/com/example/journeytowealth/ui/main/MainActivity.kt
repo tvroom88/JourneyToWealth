2c0780cacd3118636df68de58747e536a0b82878
DESKTOP-M1UFE2V\user <tvroom9102@gmail.com>
2026-02-27 11:57:46 +0900

    오류 걸리고 navigation 이동안하는 소소한 오류 수정

--- app/src/main/java/com/example/journeytowealth/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/journeytowealth/ui/main/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.example.journeytowealth.R
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -17,14 +18,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0)
             insets
         }
 
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host)
+                    as NavHostFragment
 
-        val navController = findNavController(R.id.nav_host)
+        val navController = navHostFragment.navController
+
         val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNavigation_main)
-
         bottomNav.setupWithNavController(navController)
     }
 }
